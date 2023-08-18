b47fcb3887320e290ef503ac6dcaaeac0b9072fd
annaletyflor <annaletyflor@gmail.com>
2023-08-18 09:29:04 -0300

    Tentativa de componente MovieCard Adaptações no App.jsx Criado arquivo movieCard.module.css Falta codigo movieCard.module.css

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/devflix.png";
 import searchIcon from "../assets/search.svg";
+import MovieCard from "../componentes/movieCard/movieCard";
 import "./App.css";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const apiKey = "e4d577fa";
   const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
@@ -17,11 +19,13 @@ const App = () => {
     const data = await response.json();
 
     console.log(data);
+    setMovies(data.Search);
   };
 
   const handleKeyPress = (e) => {
     e === "Enter" && searchMovies(searchTerm);
   };
+
   //   fetch(apiUrl)
   //     .then((response)=>response.json())
   //     .then((data) => console.log(data));
@@ -31,17 +35,18 @@ const App = () => {
       <div className="logo">
         <img src={logo} alt="" />
       </div>
-      <div className="search">
-        <input
-          value={searchTerm}
-          onKeyDown={handleKeyPress}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Pesquisar por filmes"
-        />
-        <img src={searchIcon} alt="Icone de pesquisa" />
-      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movies={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme foi encontrado!😨</h2>
+        </div>
+      )}
     </div>
   );
 };
-
 export default App;
